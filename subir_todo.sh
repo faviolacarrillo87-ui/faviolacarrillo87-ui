48053809ea0051275af8073d3596c47b845454ae
Faviola Carrillo <tu-correo@ejemplo.com>
2026-03-09 18:43:26 -0600

    Actualización Perfil Élite - 2026-03-09 18:43

--- subir_todo.sh
+++ subir_todo.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-
 echo "🚀 Iniciando despliegue de Élite..."
-
-# 1. Preparar archivos
 git add .
-
-# 2. Commit con fecha y hora actual
 git commit -m "Actualización Perfil Élite - $(date +'%Y-%m-%d %H:%M')"
-
-# 3. Empujar a la nube
 echo "📤 Subiendo a GitHub..."
-git push origin main
-
+# Forzamos que suba la rama master
+git push origin master
 echo "✅ ¡Perfil actualizado! Revisa tu app de GitHub ahora."
 
